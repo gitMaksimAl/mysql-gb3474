1f1b1f146c9d9dfb2f98f725145ecacd7415aed7
Maksim Aleksanin <maksim.g00gle@gmail.com>
2023-06-29 19:00:51 +0400

    Homework 6, secondary task keep doing

--- Homework_6/secondary.sql
+++ Homework_6/secondary.sql
@@ -26,12 +26,35 @@ SELECT CONCAT(firstname, ' ', lastname) AS fullname,
        hometown
 FROM users
 JOIN profiles p ON users.id = p.user_id
-WHERE hometown = (SELECT hometown FROM profiles WHERE user_id = 102) AND user_id != 102;
+WHERE hometown = (SELECT hometown
+                  FROM profiles
+                  WHERE user_id = 102) AND user_id != 102;
 
 SELECT p2.user_id FROM profiles p1
 JOIN profiles p2 ON p1.hometown = p2.hometown
 WHERE p1.user_id = 102 AND p2.user_id != 102;
-SELECT * FROM profiles p1
-JOIN profiles p2 ON p1.hometown = p2.hometown;
-SELECT * FROM users_communities uc1
-JOIN users_communities uc2 ON uc1.community_id = uc2.community_id;
+UNION
+SELECT uc2.user_id FROM users_communities uc1
+JOIN users_communities uc2 ON uc1.community_id = uc2.community_id
+WHERE uc1.user_id = 1 AND uc2.user_id != 1;
+
+#1
+SELECT initiator_user_id, target_user_id FROM friend_requests WHERE (initiator_user_id = 1 OR target_user_id = 1)
+AND status = 'approved';
+#3
+SELECT initiator_user_id, target_user_id FROM friend_requests WHERE (initiator_user_id = 3 OR target_user_id = 3)
+AND status = 'approved';
+#4
+SELECT initiator_user_id, target_user_id FROM friend_requests WHERE (initiator_user_id = 4 OR target_user_id = 4)
+AND status = 'approved';
+#10
+SELECT initiator_user_id, target_user_id FROM friend_requests WHERE (initiator_user_id = 10 OR target_user_id = 10)
+AND status = 'approved';
+
+#all
+SELECT fr.initiator_user_id AS user_id FROM friend_requests AS fr
+JOIN friends WHERE id = fr.target_user_id
+UNION
+SELECT fr.target_user_id AS user_id FROM friend_requests AS fr
+JOIN friends WHERE id = fr.initiator_user_id;
+
